187795d78cb81bafead09ca0f5c880ef09419276
Forest Nenninger <forestnenninger@Forests-MacBook-Pro.local>
2023-02-16 22:31:18 -0600

    added test validation and defined function

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -1,14 +1,26 @@
+def y_n_inputValidation(userInput):
+    while True:
+        if userInput == 'y' or userInput == 'n' or userInput == 'Y' or userInput == 'N':
+            break
+        else:
+            userInput = input('Please answer the question again using only \"y\" or \"n\": ')
+            continue
+    return userInput.lower
+
 def calculateTime():
     
     # This first line is provided for you
-    monkey_one = input("Is the first monkey smiling?:  ")
-    monkey_two = input("Is the second monkey smiling?: ")
-
+    monkey_one = input("Is the first monkey smiling? (y/n): ")
+    m1 = y_n_inputValidation(monkey_one)
+    monkey_two = input("Is the second monkey smiling? (y/n): ")
+    m2 = y_n_inputValidation(monkey_two)
+    if m1 != m2: print('Yay! We\'re going to have a good day!')
+    else: print('Uh Oh! We\'re in trouble!')
     # end assignment
-
+calculateTime()
 
 ## If you want to test locally run > python monkeyCalculator.py
 
-if __name__ == "__main__":
-    calculateTime()
+#if __name__ == "__main__":
+#    calculateTime()
     
