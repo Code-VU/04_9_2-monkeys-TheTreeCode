02372362093fe3b494e7b4c53f138698b2e3c8a2
Forest Nenninger <forestnenninger@Forests-MacBook-Pro.local>
2023-02-16 22:46:01 -0600

    un-commented code at the bottom that makes pytest work. Also removed func call in my program

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -17,10 +17,9 @@ def calculateTime():
     if m1 != m2: print('Yay! We\'re going to have a good day!')
     else: print('Uh Oh! We\'re in trouble!')
     # end assignment
-calculateTime()
 
 ## If you want to test locally run > python monkeyCalculator.py
 
-#if __name__ == "__main__":
-#    calculateTime()
+if __name__ == "__main__":
+    calculateTime()
     
